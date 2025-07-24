a1439c3150c8370ff382069856850efe11a7c445
florian.laine <florian.laine@epita.fr>
2025-07-24 02:45:50 +0200

    fix: now follows treesitter api

--- lua/cpp-tools/method_creator.lua
+++ lua/cpp-tools/method_creator.lua
@@ -5,70 +5,68 @@ local qs_classes = [[
 ]]
 
 function getClasses(buffer)
-    local parser = ts.get_parser(buffer)
-    local tree = parser:parse()[1]
-    local root = tree:root()
-    local lang = parser:lang()
-    local query = ts.query.parse(lang, qs_classes)
-    local classes = {}
-    for _, match, _ in query:iter_matches(root, buffer) do
-        local class_name, line
-        for id, node in pairs(match) do
-            local name = query.captures[id]
-            if name == "name" then
-                class_name = ts.get_node_text(node, buffer)
-            elseif name == "class" then
-                line = node:end_()
-            end
-        end
-        classes[class_name] = {
-            name = class_name,
-            line = line,
-        }
-    end
-    return classes
+	local parser = ts.get_parser(buffer)
+	local tree = parser:parse()[1]
+	local root = tree:root()
+	local lang = parser:lang()
+	local query = ts.query.parse(lang, qs_classes)
+	local classes = {}
+	local class_name, line
+	for id, node, metadata, match in query:iter_captures(root, buffer) do
+		local name = query.captures[id]
+		if name == "name" then
+			class_name = ts.get_node_text(node, buffer)
+			classes[class_name] = {
+				name = class_name,
+				line = line,
+			}
+		elseif name == "class" then
+			line = node:end_()
+		end
+	end
+	return classes
 end
 
 M.create_method = function()
-    local hh_buf = get_hh_buff()
-    if not hh_buf then
-        return
-    end
-    local method_node = require("cpp-tools.method_retriever").get_hovered()
-    if method_node == nil then
-        print("Method not found")
-        return
-    end
-    local cc_buf = vim.api.nvim_get_current_buf()
-    local base_namespace = get_current_namespace()
-    local method = parse_meth(method_node, cc_buf, base_namespace)
-    local classes = getClasses(hh_buf)
-    require("cpp-tools.menu").show_menu(table_to_list(classes), function(class)
-        if class == nil then
-            return
-        end
-        require("cpp-tools.menu").show_menu(
-            { { key = "public" }, { key = "private" }, { key = "protected" } },
-            function(access)
-                local mth_str = mth_to_str(method, {
-                    const = true,
-                })
-                local line = class.line
-                vim.api.nvim_buf_set_lines(hh_buf, line, line, false, { access.key .. ":", mth_str .. ";" })
-                vim.api.nvim_buf_call(hh_buf, function()
-                    vim.cmd(":w")
-                    vim.cmd(
-                        ":lua vim.lsp.buf.format({ async = true, range = { start = { "
-                        .. line - 1
-                        .. ', 0 }, ["end"] = { '
-                        .. line + 1
-                        .. " , 0 } } })"
-                    )
-                end)
-            end,
-            "What accesibility ?"
-        )
-    end, "In wich class ?")
+	local hh_buf = get_hh_buff()
+	if not hh_buf then
+		return
+	end
+	local method_node = require("cpp-tools.method_retriever").get_hovered()
+	if method_node == nil then
+		print("Method not found")
+		return
+	end
+	local cc_buf = vim.api.nvim_get_current_buf()
+	local base_namespace = get_current_namespace()
+	local method = parse_meth(method_node, cc_buf, base_namespace)
+	local classes = getClasses(hh_buf)
+	require("cpp-tools.menu").show_menu(table_to_list(classes), function(class)
+		if class == nil then
+			return
+		end
+		require("cpp-tools.menu").show_menu(
+			{ { key = "public" }, { key = "private" }, { key = "protected" } },
+			function(access)
+				local mth_str = mth_to_str(method, {
+					const = true,
+				})
+				local line = class.line
+				vim.api.nvim_buf_set_lines(hh_buf, line, line, false, { access.key .. ":", mth_str .. ";" })
+				vim.api.nvim_buf_call(hh_buf, function()
+					vim.cmd(":w")
+					vim.cmd(
+						":lua vim.lsp.buf.format({ async = true, range = { start = { "
+							.. line - 1
+							.. ', 0 }, ["end"] = { '
+							.. line + 1
+							.. " , 0 } } })"
+					)
+				end)
+			end,
+			"What accesibility ?"
+		)
+	end, "In wich class ?")
 end
 
 return M
--- lua/cpp-tools/method_retriever.lua
+++ lua/cpp-tools/method_retriever.lua
@@ -20,100 +20,97 @@ local qs_cc = [[
 ]]
 
 local function get_method(buffer, qs, base_namespace)
-    local parser = ts.get_parser(buffer)
-    local tree = parser:parse()[1]
-    local root = tree:root()
-    local lang = parser:lang()
-    local query = ts.query.parse(lang, qs)
-    local methods = {}
-    for _, match, _ in query:iter_matches(root, buffer) do
-        local meth, class = {}, nil
-        for id, node in pairs(match) do
-            local name = query.captures[id]
-            if name == "meth" then
-                meth = parse_meth(node, buffer, base_namespace)
-            end
-            if name == "class" then
-                class = t(node, buffer)
-            end
-        end
-        if meth then
-            if class then
-                meth["class"] = class
-                table.insert(meth["namespace"], class)
-            end
-            methods[mth_to_str(meth)] = meth
-        end
-    end
-    return methods
+	local parser = ts.get_parser(buffer)
+	local tree = parser:parse()[1]
+	local root = tree:root()
+	local lang = parser:lang()
+	local query = ts.query.parse(lang, qs)
+	local methods = {}
+	local meth, class = {}, nil
+	for id, node, metadata, match in query:iter_captures(root, buffer) do
+		local name = query.captures[id]
+		if name == "meth" then
+			meth = parse_meth(node, buffer, base_namespace)
+			if class then
+				meth["class"] = class
+				table.insert(meth["namespace"], class)
+			end
+			methods[mth_to_str(meth)] = meth
+		end
+		if name == "class" then
+			class = t(node, buffer)
+		end
+	end
+	return methods
 end
 
 function M.cc_methods(cc_buffer, base_namespace)
-    return get_method(cc_buffer, qs_cc, base_namespace)
+	return get_method(cc_buffer, qs_cc, base_namespace)
 end
 
 function M.hh_methods(hh_buffer, base_namespace)
-    return get_method(hh_buffer, qs_hh, base_namespace)
+	return get_method(hh_buffer, qs_hh, base_namespace)
 end
 
 function M.retrieve_unimplemented(hh_buffer, cc_buffer)
-    local cur_namespace = get_current_namespace()
-    local implemented = M.cc_methods(cc_buffer, cur_namespace)
-    local all = M.hh_methods(hh_buffer, cur_namespace)
-    for k, _ in pairs(implemented) do
-        all[k] = nil
-    end
-    local ret = {}
-    for k, _ in pairs(all) do
-        ret[mth_to_str(all[k], {
-            static = true,
-            const = true,
-        })] = all[k]
-    end
-    return ret
+	local cur_namespace = get_current_namespace()
+	local implemented = M.cc_methods(cc_buffer, cur_namespace)
+	local all = M.hh_methods(hh_buffer, cur_namespace)
+	for k, _ in pairs(implemented) do
+		all[k] = nil
+	end
+	local ret = {}
+	for k, _ in pairs(all) do
+		ret[mth_to_str(all[k], {
+			static = true,
+			const = true,
+		})] = all[k]
+	end
+	return ret
 end
 
 function M.retrieve_methods()
-    local hh_buffer = get_hh_buff()
-    if not hh_buffer then
-        return
-    end
-    local cc_buffer = vim.api.nvim_get_current_buf()
-    local line = vim.fn.line(".")
-    local unimplemented = M.retrieve_unimplemented(hh_buffer, cc_buffer)
-    local choices = table_to_list(unimplemented)
-    if #choices == 0 then
-        print("No method to implement")
-        return
-    end
-    table.insert(choices, 1, { key = "All" })
-    require("cpp-tools.menu").show_menu(choices, function(sel)
-        local key = sel["key"]
-        local to_implmt = { [key] = sel }
-        if key == "All" then
-            to_implmt = unimplemented
-        end
-        local impl_lines = {}
-        for _, v in pairs(to_implmt) do
-            if #impl_lines ~= 0 then
-                table.insert(impl_lines, "")
-            end
-            table.insert(impl_lines, mth_to_str(v, { namespace = true }))
-            table.insert(impl_lines, "{")
-            table.insert(impl_lines, "	")
-            table.insert(impl_lines, "}")
-        end
-        vim.api.nvim_buf_set_text(cc_buffer, line - 1, 0, line - 1, 0, impl_lines)
-        vim.api.nvim_win_set_cursor(vim.api.nvim_get_current_win(), { line, 0 })
-    end, "Chose method to implement")
+	local hh_buffer = get_hh_buff()
+	if not hh_buffer then
+		return
+	end
+	local cc_buffer = vim.api.nvim_get_current_buf()
+	local line = vim.fn.line(".")
+	local unimplemented = M.retrieve_unimplemented(hh_buffer, cc_buffer)
+	local choices = table_to_list(unimplemented)
+	if #choices == 0 then
+		print("No method to implement")
+		return
+	end
+	table.insert(choices, 1, { key = "All" })
+	require("cpp-tools.menu").show_menu(choices, function(sel)
+		local key = sel["key"]
+		local to_implmt = { [key] = sel }
+		if key == "All" then
+			to_implmt = unimplemented
+		end
+		local impl_lines = {}
+		for _, v in pairs(to_implmt) do
+			if #impl_lines ~= 0 then
+				table.insert(impl_lines, "")
+			end
+			table.insert(impl_lines, mth_to_str(v, { namespace = true }))
+			table.insert(impl_lines, "{")
+			table.insert(impl_lines, "	")
+			table.insert(impl_lines, "}")
+		end
+		vim.api.nvim_buf_set_text(cc_buffer, line - 1, 0, line - 1, 0, impl_lines)
+		vim.api.nvim_win_set_cursor(vim.api.nvim_get_current_win(), { line, 0 })
+	end, "Chose method to implement")
 end
 
 function M.get_hovered()
-    local node = ts.get_node()
-    while node and node:type() ~= "function_definition" do
-        node = node:parent()
-    end
-    return node
+	ts.get_parser():parse()
+	local node = ts.get_node()
+	while node and node:type() ~= "function_definition" do
+		node = node:parent()
+	end
+	return node
 end
 
 return M
